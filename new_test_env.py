60a8e3bd9ea8907f65c7a6593491291b2b6562d0
Jpcp98 <55485854+Jpcp98@users.noreply.github.com>
2019-12-14 13:48:37 +0000

    Update new_test_env.py
    ADDED: new tests

--- new_test_env.py
+++ new_test_env.py
@@ -1,7 +1,7 @@
 from cifo.custom_problem.travel_salesman_problem import (
     TravelSalesmanProblem, tsp_decision_variables_example, tsp_encoding_rule)
 
-from cifo.algorithm.ga_operators import (RouletteWheelSelection, initialize_randomly)
+from cifo.algorithm.ga_operators import (RouletteWheelSelection, initialize_randomly, swap_mutation,  single_point_mutation, cycle_crossover)
 
 
 tsp1 = TravelSalesmanProblem(tsp_decision_variables_example, {}, tsp_encoding_rule)
@@ -9,6 +9,8 @@ s1 = tsp1.build_solution()
 if tsp1.is_admissible(s1):
     tsp1.evaluate_solution(s1)
 print(s1)
+
+print("SOLUTION REP ", s1.representation)
 
 test_pop1 = initialize_randomly(tsp1, 10)
 
@@ -19,4 +21,15 @@ rw = RouletteWheelSelection()
 rw1 = rw.select(test_pop1, tsp1._objective,'')
 
 print("=========================================== Roulette Wheel Selection ===================================")
-print('{', rw1[0], '}', ' and ', '{', rw1[1], '}')
+print('{', rw1[0], '}')
+print('{', rw1[1], '}')
+
+#swpm1 = swap_mutation(tsp1, rw1[0])
+
+#print("=========================================== Swap Mutation ===================================")
+#print(swpm1)
+
+cc1 = cycle_crossover(tsp1, rw1[0], rw1[1])
+
+print("=========================================== Cycle Crossover ===================================")
+print('{', cc1[0], '}', ' and ', '{', cc1[1], '}')
